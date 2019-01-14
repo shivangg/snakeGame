e05e92d7f1456c8b8a644feb61d220ffa3f1f4e9
shivangg <xshivangx@gmail.com>
2019-01-15 00:36:59 +0530

    Updates

---
+++ precache-manifest.41ee45ac13818b191d27383782142e7e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "75b4529ca8130902658d",
+    "url": "shivangg.github.io/snakeGame/static/css/main.622fa019.chunk.css"
+  },
+  {
+    "revision": "75b4529ca8130902658d",
+    "url": "shivangg.github.io/snakeGame/static/js/main.75b4529c.chunk.js"
+  },
+  {
+    "revision": "c040b3cbe225736b1e2f",
+    "url": "shivangg.github.io/snakeGame/static/js/1.c040b3cb.chunk.js"
+  },
+  {
+    "revision": "2e9823d687e183089817",
+    "url": "shivangg.github.io/snakeGame/static/js/runtime~main.2e9823d6.js"
+  },
+  {
+    "revision": "c65c413a7ba65e6ff30c4ce6f7b82801",
+    "url": "shivangg.github.io/snakeGame/index.html"
+  }
+];
