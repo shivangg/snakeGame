181a0faba959676777e452395a4b74e3ef34d6ae
shivangg <xshivangx@gmail.com>
2019-01-22 19:48:05 +0530

    fix: #6 queries screensize to produce square playing field

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -7,19 +7,50 @@ class Game extends React.Component {
   constructor(props) {
     super(props);
     this.sizes = {
-      platform: {
-        width: '50%',
-        height: '50%'
-      },
-      snake: {
-        width: 5,
-        height: 5
-      }
+      platform: this.computePlayingField(window.innerHeight, window.innerWidth),
+      snake: 2
     };
     // initial state
     this.state = {
       direction: 'ArrowRight',
     }
+    this.computePlayingField = this.computePlayingField.bind(this);
+  }
+
+  computePlayingField(screenHeight, screenWidth) {
+    let padding = 0.2;
+    let width;
+    let height;
+    let fieldLeftEdge;
+    let fieldTopEdge;
+
+    let shorter = Math.min(screenHeight, screenWidth);
+    let longer = Math.max(screenHeight, screenWidth);
+
+    let longerWidth = shorter === screenHeight;
+
+    // leave 20% on each side of the shorter dimension
+    // for longer width screens
+    if (longerWidth) {
+      width = shorter * (1 - 2 * padding) ;
+      height = width;
+      fieldLeftEdge = longer / 2 - width / 2 ;
+      fieldTopEdge = shorter / 2  - height / 2 ;
+    } else {
+      height = shorter * (1 - 2 * padding) ;
+      width = height;
+      fieldLeftEdge = shorter / 2 - width / 2;
+      fieldTopEdge = longer / 2 - height / 2;
+    }
+
+
+    return {
+      top: fieldTopEdge,
+      left: fieldLeftEdge,
+      width: width,
+      height: height
+    }
+
   }
 
   componentDidMount() {
@@ -49,7 +80,7 @@ class Game extends React.Component {
   render() {
     return (
       <div tabIndex="1" style={this.sizes.platform} className='Platform'>
-        <SnakeBody size={2} direction={this.state.direction} />
+        <SnakeBody size={this.sizes.snake} direction={this.state.direction} />
       </div>
     );
   }
